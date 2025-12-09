692f410d26e7a0befe3018fcdc41674011fced04
Nick Konidaris <nick.konidaris@gmail.com>
2025-12-08 19:10:31 -0800

    updated with full range of exposure times.

--- PTC.py
+++ PTC.py
@@ -4,57 +4,69 @@ import pylab as pl
 import astropy.io.fits as fits
 
 
-
-if __name__ == "__main__":
-    
-
-    dats = []
-    for i in range(6893, 6893+2):
-        fn = "data/hen%0.4i.fits" % i
-        print(fn)
-        dat = fits.open(fn)[0].data
-
-        dats.append(dat)
-
-    dats = np.array(dats)
-
-    
+def get_rois(start=50, end=50, skip=2, W=20):
+    """ Generate ROIs"""
+        
     rois = []
     
-    W = 15
-    for x in range(50, 2048-50, 6*W):
-        for y in range(50, 2048-50, 3*W):
+    for x in range(start, 2048-end, skip*W):
+        for y in range(start, 2048-end, skip*W):
             roi = (slice(x-W, x+W), slice(y-W,y+W))
             rois.append(roi)
 
+    return rois
     
-    N = int(np.ceil(np.sqrt(len(rois))))
-    pl.figure(1)
-    pl.clf()
-    dat = dats[0]
-    for ix, roi in enumerate(rois):
-        #pl.subplot(N,N,ix+1)
-        #pl.imshow(dat[roi])
-        pass
 
-        
-            
+def handle_pair(A, B):
+    rois = get_rois()
+    N = int(np.ceil(np.sqrt(len(rois))))
 
     sigs = np.zeros(len(rois))
     vars = np.zeros_like(sigs)
-    
-    A = dats[0]
-    B = dats[1]
+    mmm = np.zeros_like(sigs)
+
     AmB = A-B
     for ix, roi in enumerate(rois):
-        sigs[ix]= np.nanmedian(A[roi])
+        mn, md, mx = np.quantile(A[roi], [.05, .5, .95])
+        sigs[ix]= md
         vars[ix] = np.var(AmB[roi])/2
+        mmm[ix] = (mx-mn)/md
         
+
+    return sigs, vars, mmm
+
+
+if __name__ == "__main__":
+
+    dats = []
+    all_sigs = []
+    all_vars = []
+    all_mmms = []
+    for i in [6893, 6914, 6953, 6973]:
+        fnA = "data/hen%0.4i.fits" % i
+        fnB = "data/hen%0.4i.fits" % (i+1)
+        print(fnA,fnB)
+        A = fits.open(fnA)[0].data
+        B = fits.open(fnB)[0].data
+        sigs, vars, mmms = handle_pair(A, B)
+
+        all_sigs.append(sigs)
+        all_vars.append(vars)
+        all_mmms.append(mmms)
+        
+    sigs = np.ndarray.flatten(np.array(all_sigs))
+    vars = np.ndarray.flatten(np.array(all_vars))
+    mmms = np.ndarray.flatten(np.array(all_mmms))
+        
+    ok = (mmms < 0.5) 
+    sigs = sigs[ok]
+    vars = vars[ok]
 
     pl.figure(2)
     pl.clf()
     pl.subplot(2,1,1)
     pl.plot(sigs, vars, 'o')
+    pl.ylim(0,10_000)
     pl.xlabel("Signal [DN]")
     pl.ylabel("Variance [DN^2]")
     pl.grid(True)
@@ -64,4 +76,4 @@ if __name__ == "__main__":
     pl.ylabel("Variance [DN^2]")
     pl.grid(True)
 
-    pl.savefig("sig-var.pdf")
+    pl.savefig("sig-var-8-dec-b.pdf")
